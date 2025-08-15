b23328e1604794aa46f03ed8e00def2b1353e195
Baptiste <baptistev59@free.fr>
2025-08-15 21:28:10 +0100

    Mise en place du menu avec le burger

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const burger = document.getElementById('burger');
+const navlinks = document.getElementById('nav-links');
+
+
+burger.addEventListener('click', () => {
+    // navlinks.classList.toggle('nav-links-close');
+    // if (navlinks.style.display==='block') {
+    //     navlinks.style.display ='none';
+    // }else{
+    //     navlinks.style.display ='block';
+    // }
+
+});
