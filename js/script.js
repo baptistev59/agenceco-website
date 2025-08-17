1222d08db07273795ca58cb9f26931d0e47d68ab
Baptiste <baptistev59@free.fr>
2025-08-17 11:15:52 +0100

    mise en place du menu burger avec JSS et finalisation séq2

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
 const burger = document.getElementById('burger');
-const navlinks = document.getElementById('nav-links');
+const nav = document.getElementById('nav');
+// const barrelogo = document.getElementById('barre-logo');
 
 
 burger.addEventListener('click', () => {
-    // navlinks.classList.toggle('nav-links-close');
+    nav.classList.toggle('nav-closed');
+    // barrelogo.classList.toggle('logo-color');
     // if (navlinks.style.display==='block') {
     //     navlinks.style.display ='none';
     // }else{
